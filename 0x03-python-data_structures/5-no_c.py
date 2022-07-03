fb0d26cc67ecd8cb90e29a9fec8e031065b9ba79
callistus406 <callistus406@mail.com>
2022-07-03 13:44:03 -0700

    commit

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    return my_string.translate({ord(c): None for c in "cC"}
+    new_str = ''
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            new_str += char
+    return new_str
