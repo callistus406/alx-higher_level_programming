9464456a4022f641e839a2c49296b86193c7fdae
callistus406 <callistus406@mail.com>
2022-07-18 11:15:17 -0700

    exception handling

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(my_list[idx], end='')
             result +=1
-        except Exception as  error:
+        except Exception:
             break
 
     print("")
