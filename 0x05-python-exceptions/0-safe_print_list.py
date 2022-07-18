83d832eaaa5200dee9aa8dec41c0fe7f05dd3efb
callistus406 <callistus406@mail.com>
2022-07-18 11:19:14 -0700

    exception handling

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    result = 0
-    for idx in range(x):
+    num = 0
+    for item in range(x):
         try:
-            print(my_list[idx], end='')
-            result +=1
+            print(my_list[item], end='')
+            num += 1
         except Exception:
             break
-
     print('')
-    return result
+    return num
