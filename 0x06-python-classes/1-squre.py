66e7efc718318eb36e8a0000c2d678d31a8c8031
callistus406 <callistus406@mail.com>
2022-07-19 13:21:05 -0700

    classes

---
+++ 0x06-python-classes/1-squre.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module defines a suqare
+"""
+
+
+class Square:
+    """ Square class """
+    def __init__(self, size):
+        """ Class initialization """
+        self.__size = size
