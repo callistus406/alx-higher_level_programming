2238049ae84a0cd82e4a54ce1a0dcf42d2b1746e
callistus406 <callistus406@mail.com>
2022-08-02 14:17:25 -0700

    commit

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
 """
-Class Rectangle based in the last task
+This module implements a Square object
 """
+
+
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """init objects"""
     def __init__(self, size):
-        """Defines attributes"""
-        self.integer_validator("size", size)
+        """initialization
+        Args:
+            size (int): size
+        """
+        super().__init__(size, size)
         self.__size = size
 
-    def area(self):
-        """Computes the area of a square"""
-        return self.__size * self.__size
-
     def __str__(self):
-        """Representation of a square"""
-        return "[Rectangle] {}/{}".format(self.__size, self.__size)
+        """string representation
+        Returns:
+            str: string
+        """
+        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
