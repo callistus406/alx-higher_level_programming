267aabefb392878c185f76faa1b2df24917e32be
callistus406 <callistus406@mail.com>
2022-08-02 23:49:15 -0700

    advance task

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-"""append after module"""
+'''advance task '''
 
 
 def append_after(filename="", search_string="", new_string=""):
-    """append a text after a substring line"""
+    '''inserts a line of text to a file
+    after each line containing a specific string
+    '''
+    with open(filename, mode='r', encoding='utf-8') as f:
+        lines = f.readlines()
 
-    with open(filename, 'r') as f:
-        content = f.readlines()
-        for (index, line) in enumerate(content):
-            if line.find(search_string) != -1:
-                content.insert(index+1, new_string)
-        new_content = "".join(content)
-    f = open(filename, 'w')
-    f.write(new_content)
-    f.close()
+    with open(filename, mode='w', encoding='utf-8') as f:
+        new_lines = []
+        for i in range(len(lines)):
+            new_lines.append(lines[i])
+            if search_string in lines[i]:
+                new_lines.append(new_string)
+        f.writelines(new_lines)
