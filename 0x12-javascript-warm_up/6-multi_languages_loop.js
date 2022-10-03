146da62a1c18c216b6bde5ba5cba460e968adba4
callistus406 <callistus406@gmail.com>
2022-10-03 11:14:11 +0100

    loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+const arrayOfStrings = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+for (let i = 0; i < arrayOfStrings.length; i++) {
+  console.log(arrayOfStrings[i]);
+}
