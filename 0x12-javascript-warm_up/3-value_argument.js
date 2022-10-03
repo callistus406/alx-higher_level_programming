c1798171472de3c339c2106f89aac77141619867
callistus406 <callistus406@gmail.com>
2022-10-03 12:21:04 +0100

    fixed

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 let myArg = process.argv[2];
-if (myArg) {
-  console.log(myArg);
-} else {
+if (myArg === undefined) {
   console.log('No argument');
+} else {
+  console.log(myArg);
 }
