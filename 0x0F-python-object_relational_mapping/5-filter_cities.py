d92539379e502c454ece16ba69912fcd56767868
callistus406 <callistus406@gmail.com>
2022-10-08 20:39:46 +0100

    done

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """
 This script  takes in the name of a state
-as an argument and lists all cities.
+as an argument and lists all cities  in db
 """
 
 import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
-
+  
 
     db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
                          passwd=argv[2], db=argv[3])
