ea1188e1d28c632e91db12d6525ab6eb17ca424d
callistus406 <callistus406@gmail.com>
2022-10-10 18:42:39 +0100

    done

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 """
-This script  takes in the name of a state
-as an argument and lists all cities.
+This script lists all cities from
+the database 
 """
 
 import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
-
+    """
+    login to the database and get the cities
+    from the database.
+    """
 
     db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
                          passwd=argv[2], db=argv[3])
@@ -16,22 +19,19 @@ if __name__ == '__main__':
     with db.cursor() as cur:
         cur.execute("""
             SELECT
-                cities.id, cities.name
+                cities.id, cities.name, states.name
             FROM
                 cities
             JOIN
                 states
             ON
                 cities.state_id = states.id
-            WHERE
-                states.name LIKE BINARY %(state_name)s
             ORDER BY
                 cities.id ASC
-        """, {
-            'state_name': argv[4]
-        })
+        """)
 
         rows = cur.fetchall()
 
     if rows is not None:
-        print(", ".join([row[1] for row in rows]))
+        for row in rows:
+            print(row)
